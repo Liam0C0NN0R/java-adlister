800e7c906a71ac911900ad8ddd89879416e4f294
Joseph Fuentes <josephkfuentes@gmail.com>
2023-06-27 21:34:39 -0400

    got the stupid links to work

--- src/main/java/adlister/controllers/AdServlet.java
+++ src/main/java/adlister/controllers/AdServlet.java
@@ -11,34 +11,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
 @WebServlet(name = "AdServlet", urlPatterns = "/ads/show")
-    public class AdServlet extends HttpServlet {
-        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            // Retrieve the ad ID from the request parameter
-            long adId = Long.parseLong(request.getParameter("id"));
+public class AdServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Retrieve the ad ID from the request parameter
+        long adId = Long.parseLong(request.getParameter("id"));
 
+        // Retrieve the ad's information from the database
+        Ad ad = DaoFactory.getAdsDao().findById(adId);
 
+        if (ad != null) {
+            // Retrieve the user's information who posted the ad
+            User user = DaoFactory.getUsersDao().findById(ad.getUserId());
 
-            // Retrieve the ad's information from the database
-            Ad ad = DaoFactory.getAdsDao().findById(adId);
+            // Set the ad and user objects as attributes in the request
+            request.setAttribute("ad", ad);
+            request.setAttribute("user", user);
 
-            if (ad != null) {
-                // Retrieve the user's information who posted the ad
-                User user = DaoFactory.getUsersDao().findById(ad.getUserId());
-
-                // Set the ad and user objects as attributes in the request
-                request.setAttribute("ad", ad);
-                request.setAttribute("user", user);
-
-                // Forward the request to a JSP page that displays the ad and user information
-                request.getRequestDispatcher("/WEB-INF/ads/show.jsp").forward(request, response);
-            } else {
-                // Handle the case when the ad is not found
-                response.sendError(HttpServletResponse.SC_NOT_FOUND);
-            }
+            // Forward the request to a JSP page that displays the ad and user information
+            request.getRequestDispatcher("/WEB-INF/ads/show.jsp").forward(request, response);
+        } else {
+            // Handle the case when the ad is not found
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
         }
-
     }
-
-
+}
--- src/main/webapp/WEB-INF/ads/ad.jsp
+++ src/main/webapp/WEB-INF/ads/ad.jsp
@@ -2,25 +2,25 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <jsp:include page="/WEB-INF/partials/head.jsp">
-        <jsp:param name="title" value="Viewing All The Ads" />
-    </jsp:include>
+  <jsp:include page="/WEB-INF/partials/head.jsp">
+    <jsp:param name="title" value="Viewing All The Ads" />
+  </jsp:include>
 </head>
 <body>
 <jsp:include page="/WEB-INF/partials/navbar.jsp" />
 
 <div class="container">
-    <h1>Here Are all the ads!</h1>
+  <h1>Here Are all the ads!</h1>
 
-    <c:forEach var="ad" items="${ads}">
-        <div class="ad-card">
-            <h2><a href="/ads/show?id=${ad.id}">${ad.title}</a></h2>
-            <p>${ad.description}</p>
-        </div>
-    </c:forEach>
+  <c:forEach var="ad" items="${ads}">
+    <div class="col-md-6">
+      <h2>${ad.title}</h2>
+      <p>${ad.description}</p>
+      <a href="ad.jsp?id=${ad.id}">View Details</a> <!-- Link to individual ad page -->
+    </div>
+  </c:forEach>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 </html>
-
